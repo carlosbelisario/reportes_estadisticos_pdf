4f00976288d30e876d975b49c6b7fd4e0dff0484
Carlos Belisario <carlos.belisario.gonzalez@gmail.com>
2012-02-12 22:32:29 -0430

    commit number 3 12-2-2012

--- Pdf.php
+++ Pdf.php
@@ -1,6 +1,5 @@
 <?php
-namespace reportes_graficos_pdf;
-use \reportes_graficos_pdf\FPDF;
+require 'fpdf/fpdf.php';
 /**
  *
  * class for generate the PDF
@@ -8,17 +7,18 @@ use \reportes_graficos_pdf\FPDF;
  * @version 2.0
  */
 class Pdf extends FPDF
-{
-    /**
-     *
-     * @var Graph $graph 
-     */
-    private $graph;
+{    
     
-    public function __construct()
+    public function __construct($orientation='P', $unit='mm', $format='A4')
     {
-        
+        parent::__construct($orientation, $unit, $format);
+    } 
+    
+    public function graphPdf(array $data, array $locationImage, array $graphSize)
+    {
+        $graph = new WrapperGraph('jpgraph_pie', $graphSize);
+        $graphName = $graph->createPieGrap($data, false, 'grafico_imagen');
+        $this->Image("$graphName", $locationImage[0], $locationImage[1], $locationImage[2], $locationImage[3]);
     }
 }
-
 ?>
---
+++ graph/WrapperGraph.php
@@ -0,0 +1,87 @@
+<?php
+require_once 'jpgraph/src/jpgraph.php';
+/**
+ * @class Wrapper Graph for generate the JpGraph
+ *
+ * @author carlos belisario <carlos.belisario.gonzalez@gmail.com>
+ * @version 1.0
+ */
+class WrapperGraph 
+{    
+    /**
+     *
+     * @var Graph 
+     */
+    private $jpgrap;
+    
+    /**
+     *
+     * @var array
+     */
+    private $size;
+    
+    /**
+     *
+     * @param String $typeGraph 
+     * @param array $size array associative whit the key width and heigth     * 
+     */
+    public function __construct($typeGraph, array $size)
+    {        
+        require_once "jpgraph/src/$typeGraph.php";
+        $this->size = $size;
+        $this->jpgrap = new Graph($size['width'], $size['height']);        
+    }
+           
+    /**
+     *
+     * @param array $data  data of the graph    
+     * @param bool $show if true shows the graph, otherwise save the graphic in a image archive
+     * @param $graphName name of the graph for save
+     * @return void 
+     * @generate the LineGraph
+     */
+    public function createLineGraph(array $data, $show = true, $graphName = 'LineGraph')
+    {        
+        $this->jpgrap->SetScale('intlin');
+        if(isset($data['title'])) {
+            $this->jpgrap->title->set($data['title']);
+        }
+        $linePlot = new LinePlot($data);
+        $this->jpgrap->Add($linePlot);
+        if($show) {
+            $this->jpgrap->Stroke();
+        } else {
+            $this->jpgrap->Stroke($graphName . 'png');
+            return $nameGraph . ".png";
+        }
+    }        
+    
+    public function createPieGrap(array $datas, $show = true, $nameGraph = 'PieGraph')
+    {   
+        require_once 'jpgraph/src/jpgraph_pie3d.php';
+        foreach ($datas as $key => $value){
+            $data[] = $value[0];
+            $nombres[] = $key; 
+            $color[] = $value[1];
+        }                         
+        
+        $graph = new PieGraph($this->size['width'], $this->size['height']);        
+        if(!empty($titulo)){
+            $graph->title->Set($titulo);
+        }   
+        //Create the plot 3D
+        $p1 = new PiePlot3D($data);
+        //set the colors
+        $p1->SetSliceColors($color);        
+        $p1->SetLegends($nombres);        
+        $graph->Add($p1);        
+        if($show) {
+            $graph->Stroke();         
+        } else {
+            $graph->Stroke("$nameGraph.png");         
+            return $nameGraph . ".png";
+        }        
+    }
+    
+}        
+?>
